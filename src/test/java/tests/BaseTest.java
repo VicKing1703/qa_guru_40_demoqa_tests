dda751cda2f161b1746bdb44e90bebe77bdf563d
Viktor Korol <v1ck1ng1703@yandex.ru>
2026-02-12 22:26:04 +0300

    Add README, FormPositiveTests and rename BaseTe

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,15 +1,23 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
-public class TestBase {
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class BaseTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.browser = "chrome";
         Configuration.browserVersion = "145.0.7632.46";
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com/";
-        Configuration.timeout = 10000;
+        Configuration.baseUrl = "https://app.qa.guru";
+//        Configuration.timeout = 10000;
+    }
+
+    @AfterAll
+    static void tearDown() {
+        closeWebDriver();
     }
 }
