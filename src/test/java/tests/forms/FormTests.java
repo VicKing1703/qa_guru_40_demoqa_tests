fdd00c9bdf203cac9cb5250ae1b50619c1e01736
Viktor Korol <v1ck1ng1703@yandex.ru>
2026-02-17 13:45:04 +0300

    Extracting variables, added checks

--- src/test/java/tests/forms/FormTests.java
+++ src/test/java/tests/forms/FormTests.java
@@ -4,12 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tests.BaseTest;
 
-import java.io.File;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static tests.TestData.*;
 
 public class FormTests extends BaseTest {
 
@@ -25,57 +23,60 @@ public class FormTests extends BaseTest {
     @Test
     void successOnlyRequiredFieldsTest() {
         // Заполняем только необходимые поля
-        $("#firstName").setValue("John");
-        $("#lastName").setValue("Cena");
-        $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue("1234567890");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#genterWrapper").$(byText(sex)).click();
+        $("#userNumber").setValue(userNumber);
 
         // Кликаем по кнопке "Submit"
-        $("#submit").click();
+        $("#submit").scrollTo().click(); // добавил скролл до кнопки
 
         // Проверяем результат
         $(".modal-header").shouldHave(text("Thanks for submitting the form"));
-        $(".modal-body").$(byText("Student Name")).parent().shouldHave(text("John " + "Cena"));
-        $(".modal-body").$(byText("Gender")).parent().shouldHave(text("Male"));
-        $(".modal-body").$(byText("Mobile")).parent().shouldHave(text("1234567890"));
+        $(".modal-body").$(byText("Student Name")).parent()
+                .shouldHave(text(firstName + " " + lastName));
+        $(".modal-body").$(byText("Gender")).parent().shouldHave(text(sex));
+        $(".modal-body").$(byText("Mobile")).parent().shouldHave(text(userNumber));
     }
 
     @Test
     void successAllFieldTest() {
         // Заполняем только необходимые поля
-        $("#firstName").setValue("Hulk");
-        $("#lastName").setValue("Hogan");
-        $("#userEmail").setValue("hulkhogan@wwe.com");
-        $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue("1234567890");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        $("#genterWrapper").$(byText(sex)).click();
+        $("#userNumber").setValue(userNumber);
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").$(byText("1953")).click();
-        $(".react-datepicker__month-select").$(byText("August")).click();
-        $(".react-datepicker__month").$(byText("11")).click();
-        $("#subjectsInput").setValue("English").pressEnter();
-        $("#hobbies-checkbox-1").click();
-        $("#uploadPicture").uploadFromClasspath("hogan.jpg");
-        $("#currentAddress").setValue("Клируотер, Флорида, США (пригород Тампы)");
+        $(".react-datepicker__year-select").$(byText(yearBirth)).click();
+        $(".react-datepicker__month-select").$(byText(monthBirth)).click();
+        $(".react-datepicker__month").$(byText(dayBirth)).click();
+        $("#subjectsInput").setValue(subjects).pressEnter();
+        $("#hobbiesWrapper").$(byText(hobbies)).click();
+        $("#uploadPicture").uploadFromClasspath(imageName);
+        $("#currentAddress").setValue(currentAddress);
         $("#state").click();
-        $("#state").$(byText("NCR")).click();
+        $("#state").$(byText(state)).click();
         $("#city").click();
-        $("#city").$(byText("Delhi")).click();
+        $("#city").$(byText(city)).click();
 
         // Кликаем по кнопке "Submit"
-        $("#submit").click();
+        $("#submit").scrollTo().click(); // добавил скролл до кнопки
 
         // Проверяем результат
         $(".modal-header").shouldHave(text("Thanks for submitting the form"));
-        $(".modal-body").$(byText("Student Name")).parent().shouldHave(text("Hulk " + "Hogan"));
-        $(".modal-body").$(byText("Student Email")).parent().shouldHave(text("hulkhogan@wwe.com"));
-        $(".modal-body").$(byText("Gender")).parent().shouldHave(text("Male"));
-        $(".modal-body").$(byText("Mobile")).parent().shouldHave(text("1234567890"));
-        $(".modal-body").$(byText("Date of Birth")).parent().shouldHave(text("11 August,1953"));
-        $(".modal-body").$(byText("Subjects")).parent().shouldHave(text("English"));
-        $(".modal-body").$(byText("Hobbies")).parent().shouldHave(text("Sports"));
-        $(".modal-body").$(byText("Picture")).parent().shouldHave(text("hogan.jpg"));
-        $(".modal-body").$(byText("Address")).parent().shouldHave(text("Клируотер, Флорида, США (пригород Тампы)"));
-        $(".modal-body").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
+        $(".modal-body").$(byText("Student Name")).parent()
+                .shouldHave(text(firstName + " " + lastName));
+        $(".modal-body").$(byText("Student Email")).parent().shouldHave(text(userEmail));
+        $(".modal-body").$(byText("Gender")).parent().shouldHave(text(sex));
+        $(".modal-body").$(byText("Mobile")).parent().shouldHave(text(userNumber));
+        $(".modal-body").$(byText("Date of Birth")).parent()
+                .shouldHave(text(dayBirth + " " + monthBirth +"," + yearBirth));
+        $(".modal-body").$(byText("Subjects")).parent().shouldHave(text(subjects));
+        $(".modal-body").$(byText("Hobbies")).parent().shouldHave(text(hobbies));
+        $(".modal-body").$(byText("Picture")).parent().shouldHave(text(imageName));
+        $(".modal-body").$(byText("Address")).parent().shouldHave(text(currentAddress));
+        $(".modal-body").$(byText("State and City")).parent().shouldHave(text(state + " " + city));
 
 
     }
@@ -83,10 +84,19 @@ public class FormTests extends BaseTest {
     @Test
     void errorFieldTest() {
         // без заполнения полей сразу кликнем по кнопке Submit
-        $("#submit").click();
+        $("#submit").scrollTo().click(); // добавил скролл до кнопки
 
         // Проверяем результат
         $(".modal-header").shouldNot(visible);
+        $("#userForm").shouldHave(cssClass("was-validated"));
+        $("#firstName").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $("#lastName").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $("#userNumber").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        $$("input[name='gender']").forEach(
+                        it -> it.shouldHave(cssValue(
+                                "border-color", "rgb(220, 53, 69)"
+                        ))
+                );
     }
 
 }
